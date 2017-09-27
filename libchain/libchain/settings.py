a515d4af1e1c4e59d51256dfe6cbed4f7db24bf9
mayank11196 <mayank11195@gamil.com>
2017-09-27 19:51:19 +0530

    Added apps and changed the mysql database configs

--- libchain/libchain/settings.py
+++ libchain/libchain/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'home.apps.HomeConfig',
+    'users.apps.UsersConfig',
+    'books.apps.BooksConfig',
+    'transactions.apps.TransactionsConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,11 @@ WSGI_APPLICATION = 'libchain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'libchaindb',
+        'USER': 'djangoadmin',
+        'PASSWORD': 'mayank',
+        'HOST': 'localhost',
     }
 }
 
