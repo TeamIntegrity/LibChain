5f2c619953c9c29e379ce2933bdb4cdbe8bf23c3
mayank11196 <mayank11195@gamil.com>
2017-10-06 14:40:43 +0530

    Modified the home/views.py method to show books to users without login

--- libchain/home/views.py
+++ libchain/home/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-# Importing the BookDescription model
-from books.models import BookDescription
+# Importing the book models
+from books.models import BookDescription, BookDepartmentSubject
 
+# Importing department models
+from department.models import Department
+
+# Importing user models
+from users.models import Student, UserProfile
 
 def home(request):
     """This is the home page functions"""
 
-    books_for_cse = BookDescription.objects.filter(branch='cse')[:12]
+    # Check to see if the user is logged in
+    try:
+        student = Student.objects.get(userprofile=UserProfile.objects.get(user=request.user))
+    except:
+        student = None
 
-    context = {'books_for_cse': books_for_cse}
+    # If user is logged in show the home page according to the user data
+    if student != None:
+        books_for_cse = BookDepartmentSubject.objects.filter(department=student.department_name)
+        context = {'books_for_cse': books_for_cse}
+
+    else:
+        books_for_cse = BookDepartmentSubject.objects.all()
+        context = {'books_for_cse': books_for_cse}
 
     return render(request, 'home.html', context)
