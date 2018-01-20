be3d5eec93127d53c126e15a7d732510b8f514ff
mayank11196 <mayank11195@gamil.com>
2018-01-21 00:50:09 +0530

    Added tx_detail feature for studetns and added templates for the same

--- libchain/transactions/views.py
+++ libchain/transactions/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from users.models import UserProfile, Student
+
+from transactions.models import Transaction
 
 
 def transaction(request, type):
     """This will return the transaction detail table for
         specific users and can be filtered by the type
     """
-    pass
+    if type == "all":
+        userprofile = UserProfile.objects.get(user=request.user)
+        try:
+            student = Student.objects.get(userprofile=userprofile)
+        except:
+            student = None
+
+        if student != None:
+            tx_details = Transaction.objects.filter(student=student)
+        else:
+            return redirect("/")
+
+        context = {"tx_details": tx_details}
+        return render(request, "tx_detail.html", context)
+
+
 
 def tx_detail(request, hash):
     """This function will return the transaction detail based on the tx_hash"""
