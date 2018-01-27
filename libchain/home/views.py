4461e497e58efd07603d7d197141ac6089268329
mayank11196 <mayank11195@gamil.com>
2018-01-27 18:17:28 +0530

    Updated home.html to include more rows of books

--- libchain/home/views.py
+++ libchain/home/views.py
@@ -22,13 +22,40 @@ def home(request):
 
     base = get_base(request)
 
+    first_row = None
+    second_row = None
+    sem = None
+    department = None
+
+    if request.user.is_authenticated:
+        userprofile = UserProfile.objects.get(user=request.user)
+        if not userprofile.entity == "student":
+            pass
+        student = Student.objects.get(userprofile=userprofile)
+        sem = student.semester
+        department = student.department_name
+
+        first_row = BookDescription.objects.filter(semester=sem)
+        second_row = BookDescription.objects.filter(department=department)
+
+    # Books for branches
     books_for_cse = BookDescription.objects.filter(department__name = "Computer Science & Engineering")[:6]
     books_for_civil = BookDescription.objects.filter(department__name = "Civil Engineering")[:6]
     books_for_mech = BookDescription.objects.filter(department__name = "Mechanical Engineering")[:6]
+    books_for_et = BookDescription.objects.filter(department__name = "Electronics & Telecommunications Engineering")[:6]
+    books_for_eee = BookDescription.objects.filter(department__name = "Electrical & Electronics Engineering")[:6]
+
+    # Books for subjects
+    books_for_maths = BookDescription.objects.filter(subject__name = "Mathematics")
+    books_for_physics = BookDescription.objects.filter(subject__name = "Physics")
+    books_for_chemistry = BookDescription.objects.filter(subject__name = "Chemistry")
 
 
     context = {'books_for_cse': books_for_cse, 'books_for_civil': books_for_civil,
-                'books_for_mech': books_for_mech, "base": base, 'semesters': semesters,
-                'departments': departments, 'subjects': subjects}
+                'books_for_mech': books_for_mech, "books_for_et": books_for_et,
+                "books_for_eee": books_for_eee, "base": base, 'semesters': semesters,
+                'departments': departments, 'subjects': subjects, "first_row": first_row,
+                "second_row": second_row, "sem": sem, "branch": department, "books_for_maths": books_for_maths,
+                "books_for_chemistry": books_for_chemistry, "books_for_physics": books_for_physics}
 
     return render(request, 'home.html', context)
